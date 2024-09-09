6facc1f7181d30ffa03bb1b1c1cf5d2d250544ae
storres21 <88690048+storres21@users.noreply.github.com>
2024-09-08 20:00:53 -0500

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Sofía Torres Ramírez on 8/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
