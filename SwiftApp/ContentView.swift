7b2ec8dac0f31fac99bb9d4b22e1d79348c4bc35
storres21 <88690048+storres21@users.noreply.github.com>
2024-09-11 22:26:04 -0500

    initial commit

--- SwiftApp/ContentView.swift
+++ SwiftApp/ContentView.swift
@@ -13,7 +13,29 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Andlet")
+            Button("Log in") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Menu("Options") {
+                Button("Menu Item 1") {
+                    // Acción para Menu Item 1
+                }
+                Button("Menu Item 2") {
+                    // Acción para Menu Item 2
+                }
+                Button("Menu Item 3") {
+                    // Acción para Menu Item 3
+                }
+            }
+            .padding()
+            LazyVStack {
+                VStack 
+                {
+                    ForEach(1...5, id: \.self) { count in
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+                    }}
+            }
         }
         .padding()
     }
