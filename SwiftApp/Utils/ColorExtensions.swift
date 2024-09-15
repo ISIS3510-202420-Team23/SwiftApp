8e298c5a7f8205446adf1b7b31728c6dd4ff7731
storres21 <88690048+storres21@users.noreply.github.com>
2024-09-15 08:34:45 -0500

    App architecture

---
+++ SwiftApp/Utils/ColorExtensions.swift
@@ -0,0 +1,32 @@
+//
+//  ColorExtensions.swift
+//  SwiftApp
+//
+//  Created by Sofía Torres Ramírez on 15/09/24.
+//
+
+// ColorExtensions.swift
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 6: // #RRGGBB
+            (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 1) // Color blanco por defecto si hay error
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: 1
+        )
+    }
+}
