f8ddb29f9022d6c6d97dc9bb768d1b151ea0a769
storres21 <88690048+storres21@users.noreply.github.com>
2024-09-15 10:47:53 -0500

    Fonts configuration

--- SwiftApp/ContentView.swift
+++ SwiftApp/ContentView.swift
@@ -14,33 +14,15 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Andlet")
-            Button("Log in") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            Menu("Options") {
-                Button("Menu Item 1") {
-                    // Acción para Menu Item 1
-                }
-                Button("Menu Item 2") {
-                    // Acción para Menu Item 2
-                }
-                Button("Menu Item 3") {
-                    // Acción para Menu Item 3
-                }
-            }
-            .padding()
-            LazyVStack {
-                VStack 
-                {
-                    ForEach(1...5, id: \.self) { count in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
-                    }}
-            }
+                .font(.custom("Montserrat-Regular", size: 20))
+            Text("test")
+                .font(.custom("LeagueSpartan-Regular", size: 5))
         }
-        .padding()
+        }
     }
-}
+
 
 #Preview {
     ContentView()
 }
+
--- SwiftAppTests/SwiftAppTests.swift
+++ SwiftAppTests/SwiftAppTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import SwiftApp
+import Firebase
 
 final class SwiftAppTests: XCTestCase {
 
